0ca03e910ce11673c1636e1c4034fefeae2da7c8
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2015-11-09 17:53:26 +0000

    SYNC

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,12 @@
 <?php
 
-Route::get('/login',      ['as' => 'auth.login',     'uses' => 'LoginController@getLogin']);
-Route::post('/postLogin', ['as' => 'auth.postLogin', 'uses' => 'LoginController@postLogin']);
+Route::get('/login',
+    ['as' => 'auth.login',
+     'uses' => 'LoginController@getLogin'
+    ]);
+Route::post('/postLogin', [
+    'as' => 'auth.postLogin',
+    'uses' => 'LoginController@postLogin']);
 
 Route::get('/', function () {
     return view('welcome');
